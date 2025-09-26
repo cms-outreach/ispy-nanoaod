95799e4e3015def5e4604230beef94e296c57086
Tom McCauley <tpmccauley@gmail.com>
2025-09-26 10:18:20 +0100

    first commit of skeleton

---
+++ src/ispynanoaod/__init__.py
@@ -0,0 +1,8 @@
+"""
+ispynanoaod: A python event display for CMS NanoAOD.
+"""
+
+from .core import info
+
+__all__ = ["info"]
+__version__ = "0.1.0"
---
+++ src/ispynanoaod/core.py
@@ -0,0 +1,5 @@
+def info() -> str:
+    """
+    Return information on the package
+    """
+    return 'iSpy NanoAOD: a python event display for the CMS NanoAOD format'
