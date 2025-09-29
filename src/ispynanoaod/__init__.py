68192f612a17094e86b163cb32b125c537b7e205
Thomas McCauley <tpmccauley@gmail.com>
2025-09-29 18:10:45 +0200

    first commit of a proper package that works

--- src/ispynanoaod/__init__.py
+++ src/ispynanoaod/__init__.py
@@ -2,7 +2,19 @@
 ispynanoaod: A python event display for CMS NanoAOD.
 """
 
-from .core import info
+from .core.event_display import EventDisplay
+from .core.data_loader import DataLoader
+from .display.renderer import EventRenderer
+from .objects.objects import ObjectFactory
+from .objects.detector import DetectorGeometry
 
-__all__ = ["info"]
 __version__ = "0.1.0"
+__author__ = "Thomas McCauley"
+
+__all__ = [
+    "EventDisplay",
+    "DataLoader",
+    "EventRenderer",
+    "ObjectFactory",
+    "DetectorGeometry"
+]
--- src/ispynanoaod/core.py
+++
@@ -1,5 +0,0 @@
-def info() -> str:
-    """
-    Return information on the package
-    """
-    return 'iSpy NanoAOD: a python event display for the CMS NanoAOD format'
